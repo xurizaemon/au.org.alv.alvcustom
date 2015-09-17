69bdc6b20ab4387a97b939df55cc1923184d0205
Chris Burgess <chris@fuzion.co.nz>
2015-09-17 16:12:03 +1200

    Correct selector for CVV field

--- js/alvcustom.js
+++ js/alvcustom.js
@@ -9,11 +9,9 @@
         // On contribute form submit, do a JS pre-check for empty
         // fields and simulate a failed validation without submission.
         var firstEl = true;
-        $('input.required, input.creditcard, input.cvv2', this).each(function(i, el) {
+        $('input.required, input.creditcard, input#cvv2', this).each(function(i, el) {
           if ($(this).val() == '') {
             $(this).addClass('error');
-            // $(this).css({backgroundColor: '#fcc'});
-            // $(this).css({border: '4px solid red'});
             $(this).after('<span class="crm-error">This field is required</span>');
             $(this).closest('.crm-section').find('label').addClass('crm-error');
             if (firstEl) {
